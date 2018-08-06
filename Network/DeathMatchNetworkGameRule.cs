1cd6c3985df91ea9756f13a21ab86eb5ec25846c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-08-06 23:56:38 +0700

    Improve codes

--- Network/DeathMatchNetworkGameRule.cs
+++ Network/DeathMatchNetworkGameRule.cs
@@ -40,7 +40,6 @@ public class DeathMatchNetworkGameRule : IONetworkGameRule
     public override bool RespawnCharacter(BaseNetworkGameCharacter character, params object[] extraParams)
     {
         var targetCharacter = character as CharacterEntity;
-        var gameplayManager = GameplayManager.Singleton;
         // In death match mode will not reset score, kill, assist, death
         targetCharacter.Exp = 0;
         targetCharacter.level = 1;
