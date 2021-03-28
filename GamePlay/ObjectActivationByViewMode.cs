9c88252e679bdecc6a7703f42264870b6713dda2
Insthync <ittipon.bay@gmail.com>
2021-03-29 00:42:13 +0700

    Add helper scripts

---
+++ GamePlay/ObjectActivationByViewMode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectActivationByViewMode : MonoBehaviour
+{
+    public CharacterEntity.ViewMode viewMode;
+    public GameObject[] objects;
+
+    void Update()
+    {
+        if (BaseNetworkGameCharacter.Local == null)
+            return;
+        foreach (var obj in objects)
+        {
+            obj.SetActive((BaseNetworkGameCharacter.Local as CharacterEntity).viewMode == viewMode);
+        }
+    }
+}
---
+++ GamePlay/SimpleBillboard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleBillboard : MonoBehaviour
+{
+    public Camera targetCamera;
+
+    public Transform CacheTransform { get; private set; }
+    public Transform CacheCameraTransform { get; private set; }
+
+    private void OnEnable()
+    {
+        CacheTransform = transform;
+        SetupCamera();
+    }
+
+    private bool SetupCamera()
+    {
+        if (targetCamera == null)
+        {
+            targetCamera = Camera.main;
+            if (targetCamera != null)
+                CacheCameraTransform = targetCamera.transform;
+        }
+        return targetCamera != null;
+    }
+
+    private void LateUpdate()
+    {
+        if (!SetupCamera())
+            return;
+        CacheTransform.rotation = Quaternion.Euler(Quaternion.LookRotation(CacheCameraTransform.forward, CacheCameraTransform.up).eulerAngles);
+    }
+}
